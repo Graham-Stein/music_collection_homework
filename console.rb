a77804ebd3c655227d4a41456b5618cc9a2f7be2
Graham-Stein <graham@guavoaviation.co.uk>
2018-09-26 17:41:51 +0100

    Add more artists and albums

--- console.rb
+++ console.rb
@@ -3,11 +3,18 @@ require('pry')
 require_relative('models/artist.rb')
 require_relative('models/album.rb')
 
-# Artist.delete_all()
-# Album.delete_all()
+Album.delete_all()
+Artist.delete_all()
 
 artist1 = Artist.new({"name" => "Alabama 3"})
+artist2 = Artist.new({"name" => "Johhny Cash"})
+artist3 = Artist.new({"name" => "The Pogues"})
+artist4 = Artist.new({"name" => "Motorhead"})
 artist1.save()
+artist2.save()
+artist3.save()
+artist4.save()
+
 
 
 album1 = Album.new({
@@ -15,7 +22,42 @@ album1 = Album.new({
   "title" => "Exile on Coldharbour Lane",
   "genre" => "Alternative"
 })
+
+album2 = Album.new({
+  "artist_id" => artist2.id,
+  "title" => "Fumsom Prison",
+  "genre" => "Rock and Roll"
+})
+
+album3 = Album.new({
+  "artist_id" => artist3.id,
+  "title" => "Rum, Sodomy & the Lash",
+  "genre" => "Celtic Punk"
+})
+
+album4 = Album.new({
+  "artist_id" => artist3.id,
+  "title" => "Should I fall from grace of God",
+  "genre" => "Celtic Punk"
+})
+
+album5 = Album.new({
+  "artist_id" => artist4.id,
+  "title" => "Ace of Spades",
+  "genre" => "Rock"
+})
+
+album6 = Album.new({
+  "artist_id" => artist4.id,
+  "title" => "Overkill",
+  "genre" => "Rock"
+})
+
 album1.save()
+album2.save()
+album3.save()
+album4.save()
+album5.save()
 
 
 # binding.pry
